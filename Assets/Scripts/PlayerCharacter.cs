5dbd9cd9b40389a77409f0894d8c60593ac3785a
amonteon <monteonale@gmail.com>
2017-10-16 17:14:45 -0700

    Creates script files for teleporter and spawn

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -8,6 +8,9 @@ public class PlayerCharacter : MonoBehaviour {
 	public int curHealth = 100;
 	public int damage = 1;
 	private bool playerDead = false;
+	public Vector3 target;
+	public Transform player;
+	public static bool IsInputEnabled = true;
 
 	void Start() {
 		_health = 1;
@@ -67,6 +70,26 @@ public class PlayerCharacter : MonoBehaviour {
 		{
 				AddjustCurrentHealth(-1);
 		}
+	}
+
+	void OnTriggerEnter(Collider hit) {
+
+		if (hit.gameObject.name == "HotSpot2") {
+			GameObject.Find ("HotSpot2").SendMessage ("React2");
+			Vector3 position = player.position;
+			float distance = Random.Range(80.0f,120.0f);
+			Vector3 objPosition = position - player.forward* distance;
+			RaycastHit hits;
+			Vector3 startHit = objPosition;
+			startHit.y += 1000;
+			Physics.Raycast(startHit, Vector3.down, out hits);
+			objPosition.y = hits.point.y + 10.9f;
+			transform.position = objPosition;
+		}
+	}
+
+	void React2 () {
+		target = Random.insideUnitSphere * 85;
 	}
 
 
