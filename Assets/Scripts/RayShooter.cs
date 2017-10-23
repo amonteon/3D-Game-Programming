01d670d1a15b4c95483a8ff578f4adc49c8aa080
amonteon <monteonale@gmail.com>
2017-10-22 20:44:54 -0700

    Merging commit

--- Assets/Scripts/RayShooter.cs
+++ Assets/Scripts/RayShooter.cs
@@ -6,6 +6,9 @@ public class RayShooter : MonoBehaviour {
 	private Camera _camera;
 	public GameObject pistol;
 	public GameObject rifle;
+	[SerializeField] private GameObject bulletPrefab;
+	public float obstacleRange = 5.0f;
+	private GameObject _bullet;
 
 	void Start() {
 		_camera = GetComponent<Camera>();
@@ -14,15 +17,22 @@ public class RayShooter : MonoBehaviour {
 //		Cursor.visible = false;
 	}
 
+//	void OnGUI() {
+//		int size = 40;
+//		float posX = _camera.pixelWidth/2 - size/4;
+//		float posY = _camera.pixelHeight/2 - size/2;
+//		GUI.Label(new Rect(posX, posY, size, size), "");
+//	}
+
 	void OnGUI() {
-		int size = 40;
+		int size = 12;
 		float posX = _camera.pixelWidth/2 - size/4;
 		float posY = _camera.pixelHeight/2 - size/2;
-		GUI.Label(new Rect(posX, posY, size, size), "");
+		GUI.Label(new Rect(posX, posY, size, size), "*");
 	}
 
 	void Update() {
-		if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject()) {
+		if (Input.GetMouseButtonDown(0)) {
 			Vector3 point = new Vector3(_camera.pixelWidth/2, _camera.pixelHeight/2, 0);
 			Ray ray = _camera.ScreenPointToRay(point);
 			RaycastHit hit;
@@ -35,13 +45,49 @@ public class RayShooter : MonoBehaviour {
 					StartCoroutine(SphereIndicator(hit.point));
 				}
 			}
-		}
+			GameObject weapon = GameObject.Find ("gun2");
+			if (weapon == true){
+				Debug.Log("Bullet Change");
+				if (Physics.Raycast(ray, out hit)) {
 
+					GameObject hitsObject = hit.transform.gameObject;
+					ReactiveTarget target = hitsObject.GetComponent<ReactiveTarget>();
+					if (target != null) {
+						target.ReactToHit();
+					}
+					else{
+						StartCoroutine (BulletIndicator (hit.point));
+					}
+				}
+
+			}
+
+		}
 		if(Input.GetKeyDown(KeyCode.R))
 			Application.LoadLevel(0);
-			
 	}
 
+//	void Update() {
+//		if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject()) {
+//			Vector3 point = new Vector3(_camera.pixelWidth/2, _camera.pixelHeight/2, 0);
+//			Ray ray = _camera.ScreenPointToRay(point);
+//			RaycastHit hit;
+//			if (Physics.Raycast(ray, out hit)) {
+//				GameObject hitObject = hit.transform.gameObject;
+//				ReactiveTarget target = hitObject.GetComponent<ReactiveTarget>();
+//				if (target != null) {
+//					target.ReactToHit();
+//				} else {
+//					StartCoroutine(SphereIndicator(hit.point));
+//				}
+//			}
+//		}
+//
+//		if(Input.GetKeyDown(KeyCode.R))
+//			Application.LoadLevel(0);
+//			
+//	}
+//
 	private IEnumerator SphereIndicator(Vector3 pos) {
 
 
@@ -78,6 +124,16 @@ public class RayShooter : MonoBehaviour {
 		yield return new WaitForSeconds(1);
 
 		Destroy(cube);
+
+	}
+	private IEnumerator BulletIndicator(Vector3 pos) {
+		bulletPrefab = GameObject.FindGameObjectWithTag ("bullet");
+		bulletPrefab = Instantiate(bulletPrefab) as GameObject;
+		// sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		bulletPrefab.transform.position = pos;
+		yield return new WaitForSeconds(1);
+
+		Destroy(bulletPrefab);
 
 	}
 }
